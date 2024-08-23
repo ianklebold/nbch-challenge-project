87715946b8866ce6e93edfc9b81fda66a92f9579
ianklebold <ianstgo@gmail.com>
2024-08-23 20:41:26 -0300

    004 - Creando clase Producto

--- src/main/java/com/nbch/challenge/app/domain/Producto.java
+++ src/main/java/com/nbch/challenge/app/domain/Producto.java
@@ -1,2 +1,28 @@
-package com.nbch.challenge.app.domain;public class Producto {
+package com.nbch.challenge.app.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
+import lombok.Builder;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Builder
+@Entity(name = "producto")
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String nombre;
+    private double precio;
+    private LocalDate fechaDeCreacion;
 }
