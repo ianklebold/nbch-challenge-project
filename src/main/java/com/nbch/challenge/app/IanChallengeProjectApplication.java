782764d540af572e264884fff7ab119f013dc279
ianklebold <ianstgo@gmail.com>
2024-08-23 15:52:17 -0300

    000 - Inicializando proyecto

---
+++ src/main/java/com/nbch/challenge/app/IanChallengeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nbch.challenge.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IanChallengeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IanChallengeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nbch/challenge/app/IanChallengeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbch.challenge.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IanChallengeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
