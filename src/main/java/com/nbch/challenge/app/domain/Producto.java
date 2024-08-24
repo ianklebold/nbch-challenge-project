bed3694d6dceb30c08ac1a10ff647bcf0d27ffa2
ianklebold <ianstgo@gmail.com>
2024-08-24 20:44:27 -0300

    005 - Agregando campos de auditoria

--- src/main/java/com/nbch/challenge/app/domain/Producto.java
+++ src/main/java/com/nbch/challenge/app/domain/Producto.java
@@ -1,16 +1,20 @@
 package com.nbch.challenge.app.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.Version;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Setter;
 import lombok.Getter;
 import lombok.Builder;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -24,5 +28,12 @@ public class Producto {
     private long id;
     private String nombre;
     private double precio;
-    private LocalDate fechaDeCreacion;
+
+    @Version
+    private Integer version;
+
+    @CreationTimestamp
+    private LocalDateTime fechaCreacionProducto;
+    @UpdateTimestamp
+    private LocalDateTime fechaActualizacionProducto;
 }
