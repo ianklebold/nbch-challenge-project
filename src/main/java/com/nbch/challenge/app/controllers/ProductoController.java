cccd67d5d59d3efbee2053c6a61d00573c2cca35
ianklebold <ianstgo@gmail.com>
2024-08-25 18:06:39 -0300

    015 - Creando Endpoint para crear producto, se agregan anotaciones Swagger.

--- src/main/java/com/nbch/challenge/app/controllers/ProductoController.java
+++ src/main/java/com/nbch/challenge/app/controllers/ProductoController.java
@@ -1,19 +1,70 @@
 package com.nbch.challenge.app.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.nbch.challenge.app.domain.Producto;
+import com.nbch.challenge.app.dtos.ErrorGenerico;
+import com.nbch.challenge.app.dtos.producto.CrearProductoDto;
+import com.nbch.challenge.app.dtos.producto.ProductoDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.nbch.challenge.app.controllers.ProductoController.PRODUCTO_PATH;
 
+@Tag(
+        name = "API REST de Productos",
+        description = "API REST DEL PROYECTO PARA CREAR, ACTUALIZAR, OBTENER Y ELIMINAR PRODUCTOS"
+)
 @RestController
-@RequestMapping(PRODUCTO_PATH)
+@RequestMapping(path = PRODUCTO_PATH, produces = {MediaType.APPLICATION_JSON_VALUE})
 public class ProductoController {
 
     public static final String PRODUCTO_PATH = "/api/v1/productos";
 
-    @GetMapping
-    public void getProducto(){
+
+    @Operation(
+            summary = "API REST para Crear Producto",
+            description = " API REST que permite crear un producto"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "HTTP Status CREATED",
+                    content = @Content(
+                            schema = @Schema(implementation = ProductoDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status SERVER ERROR",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorGenerico.class)
+                    )
+            )
+    })
+    @PostMapping
+    public ResponseEntity<?> createProduct(@RequestBody CrearProductoDto crearProductoDto){
         System.out.println("Todo bien por aca!");
+
+        Producto producto = new Producto();
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ProductoDto(
+                        producto.getId(),
+                        producto.getNombre(),
+                        producto.getDescripcion(),
+                        producto.getPrecio(),
+                        producto.getFechaCreacionProducto()
+                ));
     }
 }
