fb544ef6185ac48bbfad3680167ac8da931afefa
ianklebold <ianstgo@gmail.com>
2024-08-25 20:07:06 -0300

    023 - Creando Mapper para pasar de Producto a ProductDto

--- src/main/java/com/nbch/challenge/app/mappers/producto/ProductoMapper.java
+++ src/main/java/com/nbch/challenge/app/mappers/producto/ProductoMapper.java
@@ -2,6 +2,7 @@ package com.nbch.challenge.app.mappers.producto;
 
 import com.nbch.challenge.app.domain.Producto;
 import com.nbch.challenge.app.dtos.producto.CrearProductoDto;
+import com.nbch.challenge.app.dtos.producto.ProductoDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -13,5 +14,10 @@ public interface ProductoMapper {
     @Mapping(source = "precio", target = "precio")
     Producto crearProductoDtoToProducto(CrearProductoDto crearProductoDto);
 
-
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "nombre", target = "nombre")
+    @Mapping(source = "descripcion", target = "descripcion")
+    @Mapping(source = "precio", target = "precio")
+    @Mapping(source = "fechaCreacionProducto", target = "fechaCreacionProducto")
+    ProductoDto productoToProductoDto(Producto producto);
 }
