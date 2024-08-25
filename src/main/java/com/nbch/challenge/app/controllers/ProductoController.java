231715ab50854dc1bf045afde2267dbe7b166fd1
ianklebold <ianstgo@gmail.com>
2024-08-24 21:03:26 -0300

    011 - Creando primer endpoint (prueba)

--- src/main/java/com/nbch/challenge/app/controllers/ProductoController.java
+++ src/main/java/com/nbch/challenge/app/controllers/ProductoController.java
@@ -1,4 +1,19 @@
-package com.nbch.challenge.app.controller;
+package com.nbch.challenge.app.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.nbch.challenge.app.controllers.ProductoController.PRODUCTO_PATH;
+
+@RestController
+@RequestMapping(PRODUCTO_PATH)
 public class ProductoController {
+
+    public static final String PRODUCTO_PATH = "/api/v1/productos";
+
+    @GetMapping
+    public void getProducto(){
+        System.out.println("Todo bien por aca!");
+    }
 }
