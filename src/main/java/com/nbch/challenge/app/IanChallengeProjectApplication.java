fe2bd4b05a60c7012579f66a611f794d898a66fc
ianklebold <ianstgo@gmail.com>
2024-08-24 21:00:36 -0300

    009 - Aplicando anotaciones para swagger

--- src/main/java/com/nbch/challenge/app/IanChallengeProjectApplication.java
+++ src/main/java/com/nbch/challenge/app/IanChallengeProjectApplication.java
@@ -1,9 +1,34 @@
 package com.nbch.challenge.app;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "NBCH CHALLENGE REST API DOCUMENTACION",
+				description = "Esta es la documentacion de la API REST para el challenge del NBCH. Creado por Ian Fernandez - 2024",
+				version = "v1",
+				contact = @Contact(
+						name = "Ian Fernandez",
+						email = "ianstgo@gmail.com",
+						url = "https://www.linkedin.com/in/ian-fern%C3%A1ndez-a72598179/"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.nbch.com.ar/"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Proyecto NBCH DOCUMENTACION REST API",
+				url = "https://www.nbch.com.ar/"
+		)
+)
 public class IanChallengeProjectApplication {
 
 	public static void main(String[] args) {
