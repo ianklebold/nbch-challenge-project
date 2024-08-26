cfc3a9ff74990dbdd9105725dfc5c1d716a94796
ianklebold <ianstgo@gmail.com>
2024-08-26 00:46:09 -0300

    038 - Creacion de clase utilitaria para constantes

---
+++ src/main/java/com/nbch/challenge/app/exception/ErrorConstants.java
@@ -0,0 +1,8 @@
+package com.nbch.challenge.app.exception;
+
+public class ErrorConstants {
+    public static final String PRODUCTO_NO_EXISTE_TEMPLATE = "PRODUCTO_NO_EXISTE";
+    public static final String ERROR_DESCONOCIDO_TEMPLATE = "ERROR_DESCONOCIDO";
+    public static final String ERROR_CREATION_ENTITY_TEMPLATE = "Error en la creacion de la entidad";
+    public static final String ERROR_ARGUMENTS_TEMPLATE = "Error en los argumentos enviados a la request";
+}
